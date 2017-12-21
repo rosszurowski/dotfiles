482b72d02c663224ab1d97a42f5fa99ea127db1a
Ross Zurowski <ross@rosszurowski.com>
2017-12-21 18:36:08 -0500

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,7 +43,7 @@ binaries=(
 )
 
 echo "> Installing useful binaries..."
-brew install ${binaries[@]}
+brew install "${binaries[@]}"
 
 # Install node and iojs
 echo "> Installing node and iojs..."
@@ -64,13 +64,27 @@ modules=(
 )
 
 echo "> Installing useful node modules..."
-npm install -g ${modules[@]}
+npm install -g "${modules[@]}"
 
 # Install brew cask
 echo "> Installing brew cask..."
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
 
 # Install applications
+masapps=(
+  904280696 # Things
+  775737590 # iA Writer
+  692867256 # Simplenote
+  409183694 # Keynote
+  409203825 # Numbers
+  409201541 # Pages
+  880001334 # Reeder
+)
+
+echo "> Install App Store applications..."
+mas signin "ross@rosszurowski.com"
+mas install "${masapps[@]}"
+
 apps=(
   1password
   atom
@@ -92,19 +106,8 @@ apps=(
   vlc
 )
 
-masapps=(
-  904280696 # Things
-  775737590 # iA Writer
-  692867256 # Simplenote
-  409183694 # Keynote
-  409203825 # Numbers
-  409201541 # Pages
-  880001334 # Reeder
-)
-
 echo "> Installing applications..."
-brew cask install --appdir="/Applications" ${apps[@]}
-mas install ${masapps[@]}
+brew cask install --appdir="/Applications" "${apps[@]}"
 
 # Install quicklook plugins
 qlplugins=(
@@ -114,7 +117,7 @@ qlplugins=(
 )
 
 echo "> Installing QuickLook plugins..."
-brew cask install ${qlplugins[@]}
+brew cask install "${qlplugins[@]}"
 
 qlmanage -r
 qlmanage -r cache
@@ -122,11 +125,8 @@ qlmanage -r cache
 echo "> Installing services..."
 brew tap homebrew/services
 
-echo "> Installing Input typeface..."
-TMPDIR=`mktemp -d`
-curl "http://input.fontbureau.com/build?customize&fontSelection=whole&a=0&g=0&i=serif&l=serif&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do" > $TMPDIR/input.zip
-unzip $TMPDIR/input.zip -d $TMPDIR
-mv $TMPDIR/**/*.ttf ~/Library/Fonts/
+echo "> Installing font library..."
+script/fonts-download
 
 # Clone this repo into ./dotfiles
 read -p "Choose where to clone the dotfiles repo: " -i "$HOME/Sites/dotfiles" -e CLONE_PATH
@@ -134,12 +134,12 @@ echo "> Cloning into dotfiles repo..."
 mkdir -p $CLONE_PATH
 git clone https://github.com/rosszurowski/dotfiles $CLONE_PATH
 
-echo "> Install Atom packages..."
-script/atom
-
 # Linking dotfiles
 echo "> Linking dotfiles to $HOME..."
-cd $CLONE_PATH && script/setup
+cd $CLONE_PATH && script/zsh && script/setup
+
+echo "> Install Atom packages..."
+apm install --packages-file atom/packages.txt
 
 # Remove outdated versions from the cellar
 echo "> Cleaning up..."
