6332d14e87132dfe22444ae8f262dc32e89bdfe0
Ross Zurowski <ross@rosszurowski.com>
2023-07-03 09:47:13 -0400

    Add icon replacement raycast scripts

---
+++ raycast/scripts/icon-craft.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Replace Craft Icon
+# @raycast.mode silent
+# @raycast.packageName System
+
+# Optional parameters:
+# @raycast.icon 💻
+
+# Documentation:
+# @raycast.description Swaps the Craft.app icon out with a custom one.
+
+sh "$HOME/Developer/dotfiles/script/craft-icon"
+echo "Done!"
